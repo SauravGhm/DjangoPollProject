4eb07b38875c1d39fb1f6c7b6ce4e52fbb2ca79c
Saurav Ghimire <sauravghimire@Sauravs-MacBook-Air.local>
2024-09-09 17:00:46 -0500

    another commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,3 +5,13 @@ def index(request):
     return HttpResponse("Hello world, you are at the polls index")
 
 
+def detail(request, question_id):
+    return HttpResponse("You are looking at a question %s." % question_id)
+
+def results(request, question_id):
+    response = "you are looking at the response of the questions %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on the question %s" % question_id)
+
